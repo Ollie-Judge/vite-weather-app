ab4186378b0014d8a84720ca3414fdff6cc075f0
Ollie-Judge <ollie.judge@hotmail.com>
2024-02-01 11:34:00 +0000

    changed the api call

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios
   .get(
-    "https://api.open-meteo.com/v1/forecast?latitude=52.4814&longitude=-1.8998&current=apparent_temperature,is_day,rain,showers,snowfall&hourly=temperature_2m,rain,showers,snowfall&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timeformat=unixtime&timezone=Europe%2FLondon&past_days=14&forecast_days=14"
+    "https://api.open-meteo.com/v1/forecast?latitude=52.4814&longitude=-1.8998&current=temperature_2m,apparent_temperature,is_day,rain,showers,snowfall,weather_code&hourly=temperature_2m,apparent_temperature,precipitation_probability,rain,showers,snowfall,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&timeformat=unixtime&timezone=Europe%2FLondon&past_days=14&forecast_days=14"
   )
   .then(
     (response) => {
